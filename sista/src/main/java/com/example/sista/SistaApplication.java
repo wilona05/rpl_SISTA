304e613fec5df9a8211a2606e071805ce517278e
wilona <wilona.muliadi@gmail.com>
2024-11-25 08:25:58 +0700

    Create java spring proyek

---
+++ sista/src/main/java/com/example/sista/SistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistaApplication.class, args);
+	}
+
+}
