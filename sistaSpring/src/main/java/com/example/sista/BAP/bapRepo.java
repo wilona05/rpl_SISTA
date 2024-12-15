37989330097fb686cb69ba4c0d2a750f8a06a866
angeliquegbrl <angeliquegabriella991@gmail.com>
2024-12-15 14:48:49 +0700

    Final BAP (download-upload) frontend and backend

---
+++ sistaSpring/src/main/java/com/example/sista/BAP/bapRepo.java
@@ -0,0 +1,7 @@
+package com.example.sista.BAP;
+
+public interface bapRepo {
+    isiBAP findIsiBAP(int idSidang);
+
+    nilai findNilaiBAP(int idSidang);
+}
---
+++ sistaSpring/src/main/java/com/example/sista/Users/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.sista.Users;
+
+import java.util.List;
+
+public interface UserRepository {
+    String login(String email, String password);
+    List<User> findAll();
+    User getUserByID(String noInduk);
+    boolean getStatusKoordinator(String email);
+    boolean register(User user);
+    boolean editRoleDosen(String noInduk, String newRole);
+}
